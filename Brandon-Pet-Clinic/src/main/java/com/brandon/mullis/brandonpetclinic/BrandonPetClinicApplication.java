8e34d6095335b73d9226d072ae7a389ea9e66050
Mullisb430 <71196828+Mullisb430@users.noreply.github.com>
2021-11-20 08:15:26 -0500

    Initial Project Add

---
+++ Brandon-Pet-Clinic/src/main/java/com/brandon/mullis/brandonpetclinic/BrandonPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.brandon.mullis.brandonpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrandonPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BrandonPetClinicApplication.class, args);
+    }
+
+}
---
+++ Brandon-Pet-Clinic/src/test/java/com/brandon/mullis/brandonpetclinic/BrandonPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brandon.mullis.brandonpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrandonPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
